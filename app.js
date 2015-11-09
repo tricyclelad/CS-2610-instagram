88e55c7ff3999e9488adcbfafdb09073de16f702
troenoel <noelle.troestersolbrig@aggiemail.usu.edu>
2015-11-08 18:57:52 -0700

    trying to get mine to work

---
+++ app.js
@@ -0,0 +1,21 @@
+var express 				= require('express')
+	, exphbs					= require('express-handlebars')
+	, path						= require('path')
+  , port     				= 3000
+  //uncomment this line
+//	, indexRoutes			= require('./routes/index')
+	, userRoutes			= require('./routes/userRoutes')
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRoutes)
+app.use('/user', userRoutes)
+
+app.listen(port)
+
+console.log('Server running at http:127.0.0.1:' + port + '/')
