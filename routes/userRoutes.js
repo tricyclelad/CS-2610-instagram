525b074d200fc4eeacb13e515be821872f91649d
Jake Felzien <jakefelzien21@yahoo.com>
2015-11-08 19:11:50 -0700

    new stuff changed?

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
 router.get('/dashboard', function(req, res) {
   res.render('dashboard', {
-		layout: 'auth_base',
+		//layout: 'auth_base',
     title: 'User Dashboard!',
     welcome: 'Welcome to your dashboard!'
   })
