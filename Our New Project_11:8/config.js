7801c20bf87b5fbd6940d1c4768bb90e00f48550
Jake Felzien <jakefelzien21@yahoo.com>
2015-11-08 18:37:52 -0700

    TRYING TO COMMIT AGAIN

--- Our New Project_11:8/config.js
+++
@@ -1,9 +0,0 @@
-var config = {}
-
-
-
-config.client_id = '05f1c7e51f664617977bfe6904eda211'
-config.client_secret = '28ced868d0a34d6299571373a4126269'
-config.redirect_uri = 'http://127.0.0.1:3000/auth/finalize'
-
-module.exports = config
