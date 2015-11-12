8927985531397c5005714c7102c02b78ae480e0d
Jake Felzien <jakefelzien21@yahoo.com>
2015-11-11 17:05:13 -0700

    an attempt at the search, looks pretty close guys. needs to be finished up however

--- Our_New_Project/app.js
+++ Our_New_Project/app.js
@@ -4,6 +4,7 @@ var request = require('request')
 var querystring = require('querystring')
 var session = require('express-session')
 var cfg = require('./config')
+var bodyParser = require('body-parser')
 
 var app = express()
 var ACCESS_TOKEN = ''
@@ -24,6 +25,8 @@ app.use(session({
 	saveUninitialized: true
 }))
 
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.use(express.static('public'));
 
 app.get('/authorize', function (req, res) {
@@ -39,7 +42,12 @@ app.get('/authorize', function (req, res) {
 
 })
 
-app.get('/auth/finalize', function(req, res){
+app.get('/auth/finalize', function(req, res, next){
+
+	if(req.query.error== 'access_denied'){
+		return res.redirect('/')
+	}
+
 	var post_data = {
 		client_id: cfg.client_id,
 		client_secret: cfg.client_secret,
@@ -54,6 +62,7 @@ app.get('/auth/finalize', function(req, res){
 	request.post(options, function(error, response, body) {
 		var data = JSON.parse(body)
 		req.session.access_token = data.access_token
+	//	req.session.access_token.username = data.username
 		// ACCESS_TOKEN = data.access_token
 		res.redirect('/feed')
 	})
@@ -68,25 +77,63 @@ var options = {
 
 	request.get(options, function(error,response,body){
 		var feed = JSON.parse(body)
-		console.log(body);
+		// console.log(body);
 		res.render('feed',{
 			feed: feed.data
 		})
 	})
 })
 
-app.get('/search', function(req, res) {
-var options = {
-	url: 'https://api.instagram.com/v1/users/self/feed?access_token=' +req.session.access_token
-	// ACCESS_TOKEN
-}
+// app.get('/search', function(req, res) {
+// var options = {
+// 	url: 'https://api.instagram.com/v1/users/self/feed?access_token=' +req.session.access_token
+// 	// ACCESS_TOKEN
+// }
+//
+// 	request.get(options, function(error,response,body){
+// 		var search = JSON.parse(body)
+// 		res.render('search',{
+// 			search: search.data
+// 		})
+// 	})
+// })
 
-	request.get(options, function(error,response,body){
-		var search = JSON.parse(body)
-		res.render('search',{
-			search: search.data
-		})
-	})
+app.get('/search', function(req, res){
+	res.render('search')
 })
+
+app.post('/search', function(req, res, next){
+	console.log(req.body)
+	var tagName = req.body.query
+	var options = {
+		url: 'https://api.instagram.com/v1/tags/' + tagName + '/media/recent?access_token=' +req.session.access_token
+		// ACCESS_TOKEN
+	}
+
+	request.get(options, function (error, response, body) {
+try{
+	var search = JSON.parse(body)
+	if(feed.meta.code > 200){
+		return next(search.meta.error_message);
+	}
+} catch (err){
+	return next(err)
+}
+		console.log(search)
+			res.render('search',{
+				search: search.data
+			})
+	})
+	res.send('good post')
+})
+
+
+app.use(function(err, req, res, next){
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error:{}
+	});
+});
 
 app.listen(3000)
